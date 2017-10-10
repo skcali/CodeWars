61b188b771c85ebd5728517f2dc7bfae5790637d
Sean Cali <seankenjicali@gmail.com>
2017-10-10 11:58:34 -0700

    10/10/2017

---
+++ C#/SumOfTwoLowestInt-7/SumOfTwoLowestInt-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoLowestInt_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumTwoSmallestNumbers(int[] numbers)
+            {
+                int[] SortedNumbers = numbers.OrderBy(i => i).ToArray();
+
+                return SortedNumbers[0] + SortedNumbers[1];
+            }
+
+            int[] TestNumbers = { 5, 8, 12, 19, 22 };
+
+            Console.WriteLine(sumTwoSmallestNumbers(TestNumbers));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Working on JavaScript and C# problems in CodeWars everyday while unemployeed to keep my mind sharp, practice code tests, and keep my GitHub active. 
 
 Working on these in my own IDE, either VSCode or VS2017, and writing my own tests or copying tests from the kata. You can tell which kata it is by the file or solution name then the number next to it is the level of difficulty, from 1-8, 1 being the hardest.
+
+Link to my profile: https://www.codewars.com/users/skcali
